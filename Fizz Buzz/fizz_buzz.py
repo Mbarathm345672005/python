c1c4c06eb1af55474097f1da3e72a36295b392ba
Barath M <153623368+Mbarathm345672005@users.noreply.github.com>
2025-02-13 12:44:44 +0530

    Create fizz_buzz.py
    This is a simple Python program that takes an input (n) from the user and generates a sequence of numbers up to that range (n). The program checks each number to determine if it is divisible by 3 or 5. If a number is divisible by 3, it prints "Fizz" along with the corresponding number. If a number is divisible by 5, it prints "Buzz" along with the number. If a number is divisible by both 3 and 5, it prints "FizzBuzz" along with the number. If the number is not divisible by either, it simply prints the number itself.

---
+++ Fizz Buzz/fizz_buzz.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number:"))
+for i in range(1,n+1):
+    if (i%3==0 and i%5==0):
+        print(i," fizzbuzz")
+    elif( i%3==0):
+        print(i," fizz")
+    elif(i%5==0):
+        print(i," buzz")
+    else:
+        print(i)
